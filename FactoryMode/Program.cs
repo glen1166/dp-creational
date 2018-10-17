880289c35713da17c96c14d6c6b9c1f66c95ec2a
Guangtao Zhao <zhaoguangtao@outlook.com>
2018-10-17 19:13:37 +0800

    dotnet core sample

---
+++ FactoryMode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FactoryMode.GeneralMode;
+
+namespace FactoryMode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ICar car = CarFactory.CreateCar("BMW");
+            // car.Run();
+
+            // car = CarFactory.CreateCar("Benz");
+            // car.Run();
+
+            AbstractCarFactory factory = new CarFactory();
+            ICar car = factory.CreateCar("BMW");
+            car.Run();
+        }
+    }
+}
